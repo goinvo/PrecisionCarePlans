959c47b126232798d8dcd86585bfaf905fc79c01
edwinychoi <edwin@goinvo.com>
2017-03-07 17:31:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ $(document).ready(function() {
   //check width of window and adjust navigation menu as appropriate
   var windowWidth = $(window).width();
   if (windowWidth > 730) {
+    $(".header_links").css("display", "table-cell");
     $(".header_links").show();
+  } else {
+    $("#hamburger").show();
   }
 
   //hamburger dropdown for mobile width
